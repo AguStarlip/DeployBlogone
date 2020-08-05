99929d7ee47a0654697f480aeeec26af43e1be98
Agustin <lipchak09@gmail.com>
2020-08-05 04:08:51 -0300

    deploy v2

--- config/prod.py
+++ config/prod.py
@@ -6,4 +6,4 @@ SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgres://kldegntimmzaie:e3da2dd0a20dbbf9be71472897ed14a45183f98a3473dbb4a1c123a162ab0c91@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d3n7m9044lio17'
+SQLALCHEMY_DATABASE_URI = 'postgres://zxkwnteczlnpro:09f47e077fcea7f793416dab6929549cb949585da92a78f90b643ad5ff60ef07@ec2-50-19-26-235.compute-1.amazonaws.com:5432/d4qm3c1bb9m6vj'
